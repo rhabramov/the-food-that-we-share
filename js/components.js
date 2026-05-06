bc00f0485954b8b6fd7a50562716e40e0ab2c6f7
Robert <r.h.abramov@gmail.com>
2026-05-05 22:18:40 -0400

    trying components again

---
+++ js/components.js
@@ -0,0 +1,7 @@
+async function loadComponent(id, path) {
+  const res = await fetch(path);
+  const html = await res.text();
+  document.getElementById(id).innerHTML = html;
+}
+
+loadComponent('header', '/components/header.html');
